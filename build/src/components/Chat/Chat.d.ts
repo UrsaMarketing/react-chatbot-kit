3b2fc7ccbf9c651de8b3a47913e1447280deb5fc
LeoMld <leo.mollard2@gmail.com>
2024-05-16 18:23:43 -0400

    Try fix unknown bug

--- build/src/components/Chat/Chat.d.ts
+++ build/src/components/Chat/Chat.d.ts
@@ -1,10 +1,10 @@
-import React, { SetStateAction } from 'react';
+import React, { SetStateAction, ReactElement } from 'react';
 import './Chat.css';
 import { ICustomComponents, ICustomMessage, ICustomStyles } from '../../interfaces/IConfig';
 import { IMessage } from '../../interfaces/IMessages';
 interface IChatProps {
     setState: React.Dispatch<SetStateAction<any>>;
-    customInput?: Element;
+    CustomInput?: (props?: any) => ReactElement;
     widgetRegistry: any;
     messageParser: any;
     actionProvider: any;
@@ -22,5 +22,5 @@ interface IChatProps {
     actions?: object;
     messageContainerRef: React.MutableRefObject<HTMLDivElement>;
 }
-declare const Chat: ({ state, setState, customInput, widgetRegistry, messageParser, parse, customComponents, actionProvider, botName, customStyles, headerText, customMessages, placeholderText, validator, disableScrollToBottom, messageHistory, actions, messageContainerRef, }: IChatProps) => React.JSX.Element;
+declare const Chat: ({ state, setState, CustomInput, widgetRegistry, messageParser, parse, customComponents, actionProvider, botName, customStyles, headerText, customMessages, placeholderText, validator, disableScrollToBottom, messageHistory, actions, messageContainerRef, }: IChatProps) => React.JSX.Element;
 export default Chat;
